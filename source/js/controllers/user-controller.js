269e8789f0b64936318186a4a6028a3ea30cb761
Julie Pranger <juliepranger@gmail.com>
2017-03-18 11:09:09 -0700

    images loading in, general grid working

--- source/js/controllers/user-controller.js
+++ source/js/controllers/user-controller.js
@@ -18,17 +18,15 @@ mf.controllers = mf.controllers || {};
  * @param {!angular.$window} $window module.
  * @param {!angular.$location} $location module.
  * @param {mf.services.API} API service.
- * @param {mf.services.PubSub} PubSub service.
  * @constructor
  * @ngInject
  **/
 mf.controllers.UserController =
-    function($scope, $window, $location, API, PubSub) {
+    function($scope, $window, $location, API) {
   this.$scope = $scope;
   this.$window = $window;
   this.$location = $location;
   this.API = API;
-  this.PubSub = PubSub;
   this.clientID_ =  'dcd6712d07b04d0aa6e34c1a032dd89d';
   this.accessToken = null;
   this.images;
@@ -56,9 +54,6 @@ mf.controllers.UserController.prototype.initialize = function() {
       this.parseAccessToken(value);
     }
   }.bind(this));
-
-  // Subscribe to PubSub, callback to API getAccessToken
-  this.PubSub.subscribe(this.API.getAccessToken.bind(this));
 };
 
 
@@ -81,13 +76,10 @@ mf.controllers.UserController.prototype.getUser = function() {
 mf.controllers.UserController.prototype.parseAccessToken = function(value) {
   // parse unnecessary bits, get the access token
   this.accessToken = value.replace('/#access_token=','');
-    // alert PubSub that the token exists - API.getAccessToken callback will trigger
-    this.PubSub.publish({
-    'token': this.accessToken
-  });
 
   // get the photos!
   this.API.getInstagramPhotos(this.accessToken).then(function(response) {
     console.log(response);
+    this.images = response.data.data;
   }.bind(this))
 };
--- source/js/services/api-service.js
+++ source/js/services/api-service.js
@@ -35,23 +35,11 @@ module.exports = mf.services.API;
 
 
 /**
- * PubSub callback - get token in message
- * @param {String=} message PubSub publish message
- * @return {object}
- */
-mf.services.API.prototype.getAccessToken = function(message) {
-  if (message.token) {
-    this.token = message.token;
-  }
-  return this.token;
-};
-
-
-/**
  * Gets documents based on access levels
  * @return {object}
  */
 mf.services.API.prototype.getInstagramPhotos = function(token) {
-  return this.$http.jsonp(this.$sce.trustAsResourceUrl('https://api.instagram.com/v1/users/self/?access_token=' + token), {jsonpCallbackParam: 'callback'});
-  // return this.$http.get('/api/v1/files/folders/');
+  // return Julie's last 20 photos
+  return this.$http.jsonp(this.$sce.trustAsResourceUrl('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + token), {jsonpCallbackParam: 'callback'});
+
 };
