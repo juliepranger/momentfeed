ef42f72320ebbfc83b8f94a23e84674504cce6b4
Julie Pranger <juliepranger@gmail.com>
2017-03-19 08:11:21 -0700

    update to test

--- source/js/services/api-service.js
+++ source/js/services/api-service.js
@@ -43,6 +43,6 @@ mf.services.API.prototype.getInstagramPhotos = function(token) {
   // return Julie's last 20 photos
   // since we don't have a real back end, we have to use Angular's $sce.trustAsResourceUrl
   //    because of CORS.
-  return this.$http.jsonp(this.$sce.trustAsResourceUrl('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + token), {jsonpCallbackParam: 'callback'});
+  return this.$http.jsonp(this.$sce.trustAsResourceUrl('https://api.instagram.com/v1/users/261952381/media/recent/?access_token=' + token), {jsonpCallbackParam: 'callback'});
 
 };
